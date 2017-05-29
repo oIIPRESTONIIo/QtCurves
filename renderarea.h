a062b47d7a79d0b3fe432eb8f217155ee38992d9
oIIPRESTONIIo <james_a_preston@hotmail.co.uk>
2017-05-29 21:15:23 +0100

    Code Handles Curves Dynamically

--- renderarea.h
+++ renderarea.h
@@ -12,12 +12,12 @@ public:
   QSize minimumSizeHint() const Q_DECL_OVERRIDE;
   QSize sizeHint() const Q_DECL_OVERRIDE;
 
-  enum ShapeType { Astroid, Cycloid, HuygensCycloid, HypoCycloid };
+  enum ShapeType { Astroid, Cycloid, HuygensCycloid, HypoCycloid, FutureCurve };
 
   void setBackgroundColor(QColor color) { mBackgroundColor = color; }
   QColor getBackgroundColor() const { return mBackgroundColor; }
 
-  void setShape(ShapeType shape){ mShape = shape; }
+  void setShape(ShapeType shape){ mShape = shape; on_shape_changed(); }
   ShapeType getShape () const { return mShape; }
 
  protected:
@@ -28,12 +28,22 @@ signals:
 public slots:
 
 private:
+  void on_shape_changed();
+  QPointF compute(float t);
   QPointF compute_astroid(float t);
+  QPointF compute_cycloid(float t);
+  QPointF compute_huygens(float t);
+  QPointF compute_hypo(float t);
+  QPointF compute_future_curve(float t);
 
 private:
   QColor mBackgroundColor;
   QColor mShapeColor;
   ShapeType mShape;
+
+  int mStepCount;
+  float mScale;
+  float mIntervalLength;
 };
 
 #endif // RENDERAREA_H
