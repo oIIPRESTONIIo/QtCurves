976e3c7f335947517795c8370d62fd0bc3f2d36e
oIIPRESTONIIo <james_a_preston@hotmail.co.uk>
2017-05-29 20:45:39 +0100

    Implementing Calculation of Astroid

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "renderarea.h"
 
 MainWindow::MainWindow(QWidget *parent) :
   QMainWindow(parent),
@@ -11,4 +12,28 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
   delete ui;
+}
+
+void MainWindow::on_btnAstroid_clicked()
+{
+  this->ui->renderArea->setShape(RenderArea::Astroid);
+  this->ui->renderArea->repaint();
+}
+
+void MainWindow::on_btnCycloid_clicked()
+{
+  this->ui->renderArea->setShape(RenderArea::Cycloid);
+  this->ui->renderArea->repaint();
+}
+
+void MainWindow::on_btnHuygens_clicked()
+{
+  this->ui->renderArea->setShape(RenderArea::HuygensCycloid);
+  this->ui->renderArea->repaint();
+}
+
+void MainWindow::on_btnHypo_clicked()
+{
+  this->ui->renderArea->setShape(RenderArea::HypoCycloid);
+  this->ui->renderArea->repaint();
 }
--- renderarea.h
+++ renderarea.h
@@ -9,9 +9,31 @@ class RenderArea : public QWidget
 public:
   explicit RenderArea(QWidget *parent = nullptr);
 
+  QSize minimumSizeHint() const Q_DECL_OVERRIDE;
+  QSize sizeHint() const Q_DECL_OVERRIDE;
+
+  enum ShapeType { Astroid, Cycloid, HuygensCycloid, HypoCycloid };
+
+  void setBackgroundColor(QColor color) { mBackgroundColor = color; }
+  QColor getBackgroundColor() const { return mBackgroundColor; }
+
+  void setShape(ShapeType shape){ mShape = shape; }
+  ShapeType getShape () const { return mShape; }
+
+ protected:
+  void paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
+
 signals:
 
 public slots:
+
+private:
+  QPointF compute_astroid(float t);
+
+private:
+  QColor mBackgroundColor;
+  QColor mShapeColor;
+  ShapeType mShape;
 };
 
 #endif // RENDERAREA_H
